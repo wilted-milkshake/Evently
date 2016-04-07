24ca53f729699295558934bfa1f1bca347e45f96
Connie Cheung <conniedcheung@gmail.com>
2016-04-07 11:30:18 -0700

    Add react-router, add client-side file structure

--- client/main.js
+++ client/main.js
@@ -1,5 +1,26 @@
+// Initial Event Page
+
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router');
+
+const Links = () => 
+
+
+class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+
+  }
+
+
+}
 
 ReactDOM.render(
   React.createElement('h1', null, 'Hello World!'),
