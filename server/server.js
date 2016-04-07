68792ef0ababe1817124c02f115bf5dc5f5a268e
Connie Cheung <conniedcheung@gmail.com>
2016-04-07 16:50:10 -0700

    Use react-router to create nested components by following tutorial

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var mongoose = require('mongoose');
 
 var app = express();
 
 app.use(express.static(__dirname + '/../client'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '/../client/index.html'));
+});
 
 app.use(bodyParser.json());
 
