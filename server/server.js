868bf7424a708b098497dfc2fc997783ed27c897
Hack Reactor Students <accounts@hackreactor.com>
2016-04-07 10:26:33 -0700

    Set up server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.use(express.static(__dirname + '/../client'));
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/milkshake');
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
+module.exports = app;
