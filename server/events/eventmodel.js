b98de1743bfcb7ce57fbb128cdbcd55dbdaa31c3
Danny Tunon <danieltunon@gmail.com>
2016-04-16 20:33:00 -0700

    Create a hash code for url when creating event

--- server/events/eventmodel.js
+++ server/events/eventmodel.js
@@ -1,5 +1,6 @@
-var mongoose = require('mongoose');
-var User = require('../users/usermodel');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const User = require('../users/usermodel');
 
 var EventSchema = new mongoose.Schema({
   url: String,
@@ -10,6 +11,19 @@ var EventSchema = new mongoose.Schema({
   guests: [],
   locations: [],
   chats: []
+});
+
+
+var createSha = function (eventid) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(eventid);
+  return shasum.digest('hex').slice(0, 8);
+};
+
+EventSchema.pre('save', function (next) {
+  var url = createSha(`${this.title}${this.desctiption}${this.date}`);
+  this.url = url;
+  next();
 });
 
 var eventModel = mongoose.model('events', EventSchema);
